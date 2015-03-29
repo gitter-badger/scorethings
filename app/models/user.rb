5fcd773d5d1a1eafd7aa69fd0586eeb9f0265b41
Manu Manu <manuisfunny@gmail.com>
2015-03-28 19:47:57 -0700

    adds new files I forgot to add

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User
+  include Mongoid::Document
+  field :provider, type: String
+  field :uid, type: String
+  field :name, type: String
+  field :email, type: String
+
+  def self.create_with_omniauth(auth)
+    # copied from
+    # http://railsapps.github.io/tutorial-rails-mongoid-omniauth.html
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      if auth['info']
+        user.name = auth['info']['name'] || ""
+        user.email = auth['info']['email'] || ""
+      end
+    end
+  end
+end
+
