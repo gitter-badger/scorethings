6d84f6eb5fde0606314cd20f73254db2a719e236
Manu Manu <manuisfunny@gmail.com>
2015-05-04 12:30:41 -0700

    adds spec for creating score

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,33 +1,23 @@
 class ScoresController < ApplicationController
   def create
-    thing_input = params[:thing_input]
-    if thing_input.nil?
-      render json: {
-                 error: "thing_input is required",
-                 status: :bad_request
-             }, status: :bad_request
-    end
+    thing_input = params.require(:thing_input).permit(:twitter_handle, :twitter_hashtag)
 
     twitter_service = TwitterService.new(twitter_uid: @current_user.twitter_uid)
     if !thing_input[:twitter_handle].nil?
       twitter_account = twitter_service.get_twitter_account_from_handle(thing_input[:twitter_handle])
       if twitter_account.nil?
-        render json: {
-                   error: "could not find twitter account for twitter handle #{thing_input[:twitter_handle]}",
-                   status: :bad_request
-               }, status: :bad_request
-      else
-        score = @current_user.create_score(thing_type: 'TWITTER_UID', thing_value: twitter_account.uid)
-        if !score.nil?
-          render json: {
-                     score: score,
-                     status: :created
-                 }, status: :created
-        end
+        return render json: {
+                     error: "could not find twitter account for twitter handle #{thing_input[:twitter_handle]}",
+                     status: :bad_request
+                 }, status: :bad_request
       end
-    end
 
-    # TODO is this correct HTTP code for when I don't know how to handle create request
-    render status :unprocessable_entity
+      score = @current_user.create_score(thing_type: 'TWITTER_UID', thing_value: twitter_account[:uid])
+
+      render json: {
+                 score: score,
+                 status: :created
+             }, status: :created
+    end
   end
 end
--- spec/controllers/scores_spec.rb
+++ spec/controllers/scores_spec.rb
@@ -3,12 +3,9 @@ require 'rails_helper'
 RSpec.describe ScoresController do
   # TODO clean up similar specs to keep things DRY (Don't Repeat Yourself)
 
-  # TODO complete spec, it is currently failing
-  xdescribe "POST create" do
+  describe "POST create" do
     it "should create a new score for a twitter account when given a twitter handle" do
-      twitter_account = {
-          twitter_uid: '2121'
-      }
+      twitter_account = { uid: '2121' }
       allow_any_instance_of(TwitterService).to receive(:get_twitter_account_from_handle).with('pattonoswalt').and_return(twitter_account)
       user = create(:user_alpha)
       auth_token = user.generate_auth_token.to_s
@@ -22,10 +19,13 @@ RSpec.describe ScoresController do
       @request.env['HTTP_AUTHORIZATION'] = "Bearer #{auth_token}"
 
       # copied from http://stackoverflow.com/a/14782580/4694250
-      post :create, post_data.to_json, { CONTENT_TYPE: 'application/json', ACCEPT: 'application/json' }
+      post :create, post_data, { CONTENT_TYPE: 'application/json', ACCEPT: 'application/json' }
 
       expect(response).to have_http_status(:created)
-      expect(response)
+      # TODO better way to organize specs?
+      # I think this should be separated into request specs:
+      # https://www.relishapp.com/rspec/rspec-rails/docs/request-specs/request-spec
+      expect(response.body).to include("2121")
     end
   end
 end
