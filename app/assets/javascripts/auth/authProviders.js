02354b50710a22a89ba47488e251b6483c8cd04d
Manu Manu <manuisfunny@gmail.com>
2015-06-04 12:11:54 -0700

    removes font awesome

--- app/assets/javascripts/auth/authProviders.js
+++ app/assets/javascripts/auth/authProviders.js
@@ -2,5 +2,5 @@ angular.module('app').constant('AUTH_PROVIDERS', [
     { name: 'twitter', title: 'Twitter' },
     { name: 'github', title: 'GitHub' },
     { name: 'soundcloud', title: 'Soundcloud' },
-    { name: 'google', faName: 'google-plus', title: 'Google' }
+    { name: 'google', title: 'Google' }
 ]);
