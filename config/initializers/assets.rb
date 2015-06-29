cba7c1854c6c80ea80cdaee74d7cd1fb466f1f21
Manu Manu <manuisfunny@gmail.com>
2015-06-28 17:50:08 -0700

    adds emoji to assets pipeline for prod

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,12 +1,10 @@
 # Be sure to restart your server when you modify this file.
-Rails.application.config.assets.paths << Rails.root.join('assets', 'fonts')
 Rails.application.config.assets.paths << Rails.root.join('bower_components')
 Rails.application.config.assets.enabled = true
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join('bower_components', 'angular-emoji-filter-hd', 'dist')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
